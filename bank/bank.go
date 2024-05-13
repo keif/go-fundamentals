f4d258983cad43f4044a8a05b978fca24323c0a9
Keith Baker <github@baker.is>
2024-05-13 12:22:18 -0400

    if/else examples

---
+++ bank/bank.go
@@ -0,0 +1,65 @@
+package main
+
+import "fmt"
+
+func main() {
+	var choice int
+	accountBalance := 1000.0
+
+	fmt.Println("Welcome to Go Bank!")
+
+	for {
+		fmt.Println("What do you want to do?")
+		fmt.Println("1. Check balance")
+		fmt.Println("2. Deposit money")
+		fmt.Println("3. Withdraw money")
+		fmt.Println("4. Exit")
+
+		fmt.Print("Your choice: ")
+		fmt.Scan(&choice)
+
+		checkBalance := choice == 1
+		depositMoney := choice == 2
+		withdrawMoney := choice == 3
+		exitApp := choice == 4
+
+		if checkBalance {
+			fmt.Println("Your balance is: ", accountBalance)
+		} else if depositMoney {
+			fmt.Println("Your current balance is: ", accountBalance)
+			fmt.Print("Your deposit: ")
+			var depositAmount float64
+			fmt.Scan(&depositAmount)
+
+			if depositAmount <= 0 {
+				fmt.Println("Invalid amount. Your deposit must be more than $0")
+				continue
+			}
+
+			accountBalance += depositAmount
+			fmt.Println("Your balance is now: ", accountBalance)
+		} else if withdrawMoney {
+			fmt.Println("Your current balance is: ", accountBalance)
+			fmt.Print("Your withdrawal amount: ")
+			var withdrawalAmount float64
+			fmt.Scan(&withdrawalAmount)
+
+			if withdrawalAmount <= 0 {
+				fmt.Println("Invalid amount. Your withdrawal must be more than $0")
+				continue
+			}
+
+			if withdrawalAmount > accountBalance {
+				fmt.Println("Invalid amount. You can't withdraw more than your balance.")
+			}
+
+			accountBalance -= withdrawalAmount
+			fmt.Println("Your balance is now: ", accountBalance)
+		} else if exitApp {
+			fmt.Println("Goodbye! ")
+			break
+		}
+	}
+
+	fmt.Println("Thanks for choosing our bank!")
+}
