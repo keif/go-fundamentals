c79724d940a9de7128fe4685570b57eac908ab78
Keith Baker <github@baker.is>
2024-05-13 17:29:11 -0400

    Struct Embedding

--- structs/structs.go
+++ structs/structs.go
@@ -17,6 +17,11 @@ func main() {
 		return
 	}
 
+	admin := user.NewAdmin("test@example.com", "test123")
+	admin.OutputUserDetails()
+	admin.ClearUserName()
+	admin.OutputUserDetails()
+
 	// ... do something awesome with that gathered data!
 
 	appUser.OutputUserDetails()
