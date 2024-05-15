ba068a47a24a5d0e0f2969b2f23627c44ee84851
Keith Baker <github@baker.is>
2024-05-15 17:18:09 -0400

    returning functions as values

---
+++ section-8/main.go
@@ -0,0 +1,50 @@
+package main
+
+import "fmt"
+
+type transformFn func(int) int
+
+// type anotherFn func(int, []string, map[string[p[int) ([]int, string)
+
+func main() {
+	numbers := []int{1, 2, 3, 4}
+	moreNumbers := []int{5, 1, 2}
+	doubled := transformNumbers(&numbers, double)
+	tripled := transformNumbers(&numbers, triple)
+	fmt.Println(doubled)
+	fmt.Println(tripled)
+
+	transformerFn1 := getTransformerFunction(&numbers)
+	transformerFn2 := getTransformerFunction(&moreNumbers)
+
+	transformedNumbers := transformNumbers(&numbers, transformerFn1)
+	moreTransformedNumbers := transformNumbers(&moreNumbers, transformerFn2)
+
+	fmt.Println(transformedNumbers)
+	fmt.Println(moreTransformedNumbers)
+}
+
+func transformNumbers(numbers *[]int, transform transformFn) []int {
+	tNums := []int{}
+	for _, val := range *numbers {
+		tNums = append(tNums, transform(val))
+	}
+
+	return tNums
+}
+
+func getTransformerFunction(numbers *[]int) transformFn {
+	if (*numbers)[0] == 1 {
+		return double
+	} else {
+		return triple
+	}
+}
+
+func double(number int) int {
+	return number * 2
+}
+
+func triple(number int) int {
+	return number * 3
+}
