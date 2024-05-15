796c39b427a50905abdf7fdee2699d8338ff0d7e
Keith Baker <github@baker.is>
2024-05-15 11:41:28 -0400

    selecting parts of arrays with slices

--- section-7/lists.go
+++ section-7/lists.go
@@ -3,7 +3,19 @@ package main
 import "fmt"
 
 func main() {
+	// indices 1,2,3 are nil
+	var productNames [4]string = [4]string{"A Book"}
+	// no nil values
 	prices := [4]float64{10.99, 9.99, 11.99, 12.99}
 
+	// indices 1,3 are still nil
+	productNames[2] = "A Carpet"
+	fmt.Println(productNames)
 	fmt.Println(prices)
+
+	fmt.Println(prices[0])
+
+	// a slice of prices array
+	featuredPrices := prices[1:3]
+	fmt.Println(featuredPrices)
 }
