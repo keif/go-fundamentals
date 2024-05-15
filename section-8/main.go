37109cd59b93d42761c91a318c52aaa5d89b4bd6
Keith Baker <github@baker.is>
2024-05-15 17:23:04 -0400

    introducing anonymous functions

--- section-8/main.go
+++ section-8/main.go
@@ -4,47 +4,22 @@ import "fmt"
 
 type transformFn func(int) int
 
-// type anotherFn func(int, []string, map[string[p[int) ([]int, string)
-
 func main() {
-	numbers := []int{1, 2, 3, 4}
-	moreNumbers := []int{5, 1, 2}
-	doubled := transformNumbers(&numbers, double)
-	tripled := transformNumbers(&numbers, triple)
-	fmt.Println(doubled)
-	fmt.Println(tripled)
+	numbers := []int{1, 2, 3}
 
-	transformerFn1 := getTransformerFunction(&numbers)
-	transformerFn2 := getTransformerFunction(&moreNumbers)
+	transformed := transformNumbers(&numbers, func(number int) int {
+		return number * 2
+	})
 
-	transformedNumbers := transformNumbers(&numbers, transformerFn1)
-	moreTransformedNumbers := transformNumbers(&moreNumbers, transformerFn2)
-
-	fmt.Println(transformedNumbers)
-	fmt.Println(moreTransformedNumbers)
+	fmt.Println(transformed)
 }
 
 func transformNumbers(numbers *[]int, transform transformFn) []int {
-	tNums := []int{}
+	dNumbers := []int{}
+
 	for _, val := range *numbers {
-		tNums = append(tNums, transform(val))
+		dNumbers = append(dNumbers, transform(val))
 	}
 
-	return tNums
-}
-
-func getTransformerFunction(numbers *[]int) transformFn {
-	if (*numbers)[0] == 1 {
-		return double
-	} else {
-		return triple
-	}
-}
-
-func double(number int) int {
-	return number * 2
-}
-
-func triple(number int) int {
-	return number * 3
+	return dNumbers
 }
