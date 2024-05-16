470b5c971a5e68b1d34ccb68fd321dcb864f5d60
Keith Baker <github@baker.is>
2024-05-16 15:47:33 -0400

    adding a constructor function

--- price-calculator/prices/prices.go
+++ price-calculator/prices/prices.go
@@ -5,3 +5,12 @@ type TaxIncludedPriceJob struct {
 	InputPrices       []float64          `json:"input_prices"`
 	TaxIncludedPrices map[string]float64 `json:"tax_included_prices"`
 }
+
+// constructor
+// return pointer so we only share it once in memory
+func NewTaxIncludedPriceJob(taxRate float64) *TaxIncludedPriceJob {
+	return &TaxIncludedPriceJob{
+		InputPrices: []float64{10, 20, 30},
+		TaxRate:     taxRate,
+	}
+}
