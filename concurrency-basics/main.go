6960add2158baddbaf579956650881da9ffdb3d7
Keith Baker <github@baker.is>
2024-05-16 18:23:16 -0400

    working with multiple channels and goroutines

--- concurrency-basics/main.go
+++ concurrency-basics/main.go
@@ -5,23 +5,45 @@ import (
 	"time"
 )
 
-func greet(phrase string) {
+func greet(phrase string, doneChan chan bool) {
 	fmt.Println("Hello!", phrase)
+	doneChan <- true
 }
 
 func slowGreet(phrase string, doneChan chan bool) {
 	time.Sleep(3 * time.Second) // simulate a slow, long-taking task
 	fmt.Println("Hello!", phrase)
 	doneChan <- true
+	close(doneChan) // only usable if we know it would execute last
 }
 
 func main() {
-	//go greet("Nice to meet you!")
-	//go greet("How are you?")
+	//dones := make([]chan bool, 4)
 	done := make(chan bool)
+
+	//dones[0] = make(chan bool)
+	go greet("Nice to meet you!", done)
+	//dones[1] = make(chan bool)
+	go greet("How are you?", done)
+	//dones[2] = make(chan bool)
 	go slowGreet("How ... are ... you ...?", done)
-	//go greet("I hope you're liking the course!")
+	//dones[3] = make(chan bool)
+	go greet("I hope you're liking the course!", done)
 	// go will only continue after this line of code
 	// some data has come out of the channel
-	<-done
+
+	// one <- done for each go routine, but not ideal/efficient
+	//<-done
+	//<-done
+	//<-done
+	//<-done
+	// second approach
+	//for _, done := range dones {
+	//	<-done
+	//}
+	//for doneChan := range done {
+	//fmt.Println(doneChan)
+	//}
+	for range done {
+	}
 }
