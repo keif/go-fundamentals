3d7948f4dff44faa752ad10adfb45480146c80d3
Keith Baker <github@baker.is>
2024-05-22 15:17:47 -0400

    storing data in the database (INSERT)

--- rest-api/models/event.go
+++ rest-api/models/event.go
@@ -1,11 +1,15 @@
 package models
 
-import "time"
+import (
+	"example.com/rest-api/db"
+	"time"
+)
 
 type Event struct {
 	ID          int64     `xorm:"pk autoincr"`
 	Name        string    `xorm:"VARCHAR(255)" binding:"required"`
 	Description string    `xorm:"TEXT" binding:"required"`
+	Location    string    `xorm:"TEXT" binding:"required"`
 	DateTime    time.Time `xorm:"TEXT" binding:"required"`
 	UserID      int64
 }
@@ -26,7 +30,22 @@ func GetAllEvents() (events []Event) {
 	return events
 }
 
-func (e *Event) Save() {
-	// TODO: store to database
-	events = append(events, *e)
+func (e *Event) Save() error {
+	query := `
+	INSERT INTO events (name, description, location, dateTime, user_id)
+	VALUES (?, ?, ?, ?, ?)
+    `
+	// prepare for insertion - no sql attacks
+	stmt, err := db.DB.Prepare(query)
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
+	result, err := stmt.Exec(e.Name, e.Description, e.Location, e.DateTime, e.UserID)
+	if err != nil {
+		return err
+	}
+	id, err := result.LastInsertId()
+	e.ID = id
+	return err
 }
