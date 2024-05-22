710c54b31fb78190b5af0df3e271017ce72cf720
Keith Baker <github@baker.is>
2024-05-22 13:48:07 -0400

    setting up a first route and handling a first request

--- rest-api/main.go
+++ rest-api/main.go
@@ -1,5 +1,37 @@
 package main
 
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
 func main() {
-	
+	server := gin.Default()
+
+	// GET, POST, PUT, PATCH, DELETE
+	server.GET("/", getBase)
+
+	server.GET("/ping", getPing)
+
+	server.GET("/events", getEvents)
+
+	server.Run(":8080") // localhost:8080
+}
+
+func getBase(context *gin.Context) {
+	context.JSON(http.StatusOK, gin.H{
+		"message": "ok",
+	})
+}
+
+func getEvents(context *gin.Context) {
+	context.JSON(http.StatusOK, gin.H{
+		"message": "ok",
+	})
+}
+
+func getPing(context *gin.Context) {
+	context.JSON(http.StatusOK, gin.H{
+		"message": "pong",
+	})
 }
