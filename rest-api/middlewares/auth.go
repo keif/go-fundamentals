1799c75b54bfbc2e5d7e4d988634264f75cf5107
Keith Baker <github@baker.is>
2024-05-23 16:19:35 -0400

    adding an authentication middleware

---
+++ rest-api/middlewares/auth.go
@@ -0,0 +1,24 @@
+package middlewares
+
+import (
+	"example.com/rest-api/utils"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func Authenticate(context *gin.Context) {
+	var err error
+	token := context.Request.Header.Get("Authorization")
+	if token == "" {
+		context.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "Not authorized"})
+		return
+	}
+
+	userId, err := utils.VerifyToken(token)
+	if err != nil {
+		context.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "Not Authorized."})
+		return
+	}
+
+	context.Next()
+}
